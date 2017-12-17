82b846385c6731c8dc8d7d3eb3022c5486284d41
antony-jr <antonyjr@protonmail.com>
2017-12-17 11:10:28 +0530

    adding QArchive::Reader and some examples for that!

---
+++ examples/read_archive/main.cpp
@@ -0,0 +1,54 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include "QArchive.hpp"
+
+int main(int argc, char** argv)
+{
+    QCoreApplication app(argc, argv);
+
+    /*
+     * 1.Construct
+    */
+    QArchive::Reader e("test.7z");
+
+
+    /*
+     * 2.Connect Callbacks
+    */
+
+    // emitted when all extraction is finished
+    QObject::connect(&e, &QArchive::Reader::archiveFiles, [&](QString archive , QStringList files) {
+        qDebug() << archive << " :: ";
+	qDebug() << files;
+        e.quit();
+        app.quit();
+    });
+
+    // emitted when something goes wrong
+    QObject::connect(&e, &QArchive::Reader::error, [&](short code, QString file) {
+        switch(code) {
+        case QArchive::ARCHIVE_READ_ERROR:
+            qDebug() << "unable to find archive :: " << file;
+            app.quit();
+            break;
+        case QArchive::ARCHIVE_QUALITY_ERROR:
+            qDebug() << "bad archive! :: " << file;
+            app.quit();
+            break;
+        case QArchive::ARCHIVE_UNCAUGHT_ERROR:
+            qDebug() << "fatal error. :: " << file;
+            app.quit();
+            break;
+        default:
+            qDebug() << "unknown error. :: " << file;
+            app.quit();
+            break;
+        }
+    });
+
+    /*
+     * 3.Start extraction!
+    */
+    e.start();
+    return app.exec();
+}
