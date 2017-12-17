de240dc31e68b4f41fc632d503b34247db0b596e
antony-jr <antonyjr@protonmail.com>
2017-12-17 14:16:27 +0530

    correcting a nitpick!

--- QArchive.hpp
+++ QArchive.hpp
@@ -10,9 +10,9 @@
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
  *
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
  *
  * * Neither the name of the copyright holder nor the names of its
  *   contributors may be used to endorse or promote products derived from
