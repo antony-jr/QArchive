e3aea9f236dd2346b171de7fa007a0e0a63d2c38
antony-jr <antonyjr@protonmail.com>
2017-12-28 04:09:46 +0530

    correcting install.sh!

--- install.sh
+++ install.sh
@@ -11,18 +11,18 @@ repoRawUrl="https://raw.githubusercontent.com/antony-jr/QArchive/master/"
 
 if [ $isCurl ]
 	then
-		echo "Installing " $packageName
-		echo "Creating directory:: " $packageName
+		echo Installing  $packageName
+		echo Creating directory:: $packageName
 		mkdir $packageName
-		echo "Downloading LICENSE and the latest files... "
-		curl -L $repoRawUrl$packageName".hpp" --output $packageName"/"$packageName".hpp"
-		curl -L $repoRawUrl$license --output $packageName"/"$license
-		echo "Installation complete!"
-		echo "Thank you for choosing "$packageName
+		echo Downloading LICENSE and the latest files... 
+		curl -L $repoRawUrl$packageName.hpp --output $packageName/$packageName.hpp
+		curl -L $repoRawUrl$license --output $packageName/$license
+		echo Installation complete!
+		echo Thank you for choosing $packageName
 		exit
 		
 	else
-		echo "Please install curl to continue the installation."
-		echo "Aborting... "
+		echo Please install curl to continue the installation.
+		echo Aborting... 
 		exit
 fi
