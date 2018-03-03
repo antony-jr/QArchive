9035d10180b109e849dd68d6022752a87118b9a8
antony-jr <antonyjr@protonmail.com>
2018-03-04 03:35:04 +0530

    Welcome 2017 , I guess its late but yea!

--- src/QArchive.cpp
+++ src/QArchive.cpp
@@ -1,7 +1,7 @@
 /*
  * BSD 3-Clause License
  *
- * Copyright (c) 2017, Antony jr
+ * Copyright (c) 2018, Antony jr
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
