266b62524dc771d6b618be17380425fc980f9022
Rosen Penev <rosenp@gmail.com>
2022-12-01 22:35:39 -0800

    tests: remove QScopedPointer here too
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- tests/TestRunner.cc
+++ tests/TestRunner.cc
@@ -8,37 +8,37 @@ TestRunner::TestRunner() {
     m_TempDir.reset(new QTemporaryDir);
     m_Future.reset(new QFuture<void>);
     m_FutureWatcher.reset(new QFutureWatcher<void>);
-    m_CompressorTests.reset(new QArchiveDiskCompressorTests(m_TempDir.data()));
-    m_ExtractorTests.reset(new QArchiveDiskExtractorTests(m_TempDir.data()));
-    m_MemoryExtractorTests.reset(new QArchiveMemoryExtractorTests(m_TempDir.data()));
-    m_MemoryCompressorTests.reset(new QArchiveMemoryCompressorTests(m_TempDir.data()));
+    m_CompressorTests.reset(new QArchiveDiskCompressorTests(m_TempDir.get()));
+    m_ExtractorTests.reset(new QArchiveDiskExtractorTests(m_TempDir.get()));
+    m_MemoryExtractorTests.reset(new QArchiveMemoryExtractorTests(m_TempDir.get()));
+    m_MemoryCompressorTests.reset(new QArchiveMemoryCompressorTests(m_TempDir.get()));
 
-    connect(m_FutureWatcher.data(), &QFutureWatcher<void>::finished,
+    connect(m_FutureWatcher.get(), &QFutureWatcher<void>::finished,
             this, &TestRunner::finished, Qt::DirectConnection);
 }
 
 TestRunner::~TestRunner() = default;
 
 void TestRunner::start() {
-    *(m_Future.data()) = QtConcurrent::run([&]() {
+    *m_Future = QtConcurrent::run([this] {
         runTests();
         return;
     });
-    m_FutureWatcher->setFuture(*(m_Future.data()));
+    m_FutureWatcher->setFuture(*m_Future);
 
 }
 
 void TestRunner::runTests() {
     // Run the compressor tests which should generate the
     // desired archives to test it with the extractor.
-    QTest::qExec(m_CompressorTests.data());
+    QTest::qExec(m_CompressorTests.get());
 
     // Run disk extractor tests.
-    QTest::qExec(m_ExtractorTests.data());
+    QTest::qExec(m_ExtractorTests.get());
 
     // Run memory extractor tests
-    QTest::qExec(m_MemoryExtractorTests.data());
+    QTest::qExec(m_MemoryExtractorTests.get());
 
     // Run memory compressor tests
-    QTest::qExec(m_MemoryCompressorTests.data());
+    QTest::qExec(m_MemoryCompressorTests.get());
 }
