e67bdb538d2d592b570d6ae81e78d84e1fe1ee05
antony-jr <antonyjr@protonmail.com>
2023-12-11 11:19:00 +0530

    [Release v2.2.9] QArchive v2.2.9 release
    This is a minor release, no need to update to this version.
    
    # Changelog
    
       * Added QARCHIVE_VERSION_STR symbolic constant to report
         QArchive version to the users.
    
       * Fix tests
    
    # CI/CD Changes
    
       * Added prebuilds for Windows Users, MSVC x64 builds will
         be uploaded to the new releases automatically.
    
       * Also upload prebuilt library of each commit to continuous
         tag for users to test and consume.
    
    Signed-off-by: Divya Antony J R <antonyjr@protonmail.com>

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -15,9 +15,9 @@ sed -i 's/QArchive VERSION .*/QArchive VERSION '$QARCHIVE_VERSION'\)/' CMakeList
 sed -i "s/  version: .*/  version: '$QARCHIVE_VERSION',/" meson.build
 
 cat > /tmp/qarchive.commit.template.txt << EOF
-[Release $QARCHIVE_VERSION] QArchive v$QARCHIVE_VERSION release
+[Release v$QARCHIVE_VERSION] QArchive v$QARCHIVE_VERSION release
 
-#Changelog
+# Changelog
 
    * Change 1
    * Change 2
@@ -30,7 +30,9 @@ git status
 
 read -p "Do you want to commit? press any key to continue..."
 
+git config --local core.commentchar ";"
 git commit -S -t /tmp/qarchive.commit.template.txt
+git config --local core.commentchar "#"
 
 rm -rf /tmp/qarchive.commit.template.txt
 
